b4c5c21abb8d457159283fffc64deab37a3e4190
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-04-29 10:33:35 +0100

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    name: "flutter-blog",
-    themeColor: "#000",
-    msTileColor: "#fff",
-    appleMobileWbeAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "#fff",
-    workboxPluginMode: "GenerateSW",
-    workboxOptions: {
-      // swSrc: "./service-worker.js",
-      exclude: [/_redirect/, /\.map$/, /_headers/],
-    },
-    manifestOptions: {
-      background_color: "#fff",
-    },
-  },
+  // pwa: {
+  //   name: "flutter-blog",
+  //   themeColor: "#000",
+  //   msTileColor: "#fff",
+  //   appleMobileWbeAppCapable: "yes",
+  //   appleMobileWebAppStatusBarStyle: "#fff",
+  //   workboxPluginMode: "GenerateSW",
+  //   workboxOptions: {
+  //     // swSrc: "./service-worker.js",
+  //     exclude: [/_redirect/, /\.map$/, /_headers/],
+  //   },
+  //   manifestOptions: {
+  //     background_color: "#fff",
+  //   },
+  // },
 });
